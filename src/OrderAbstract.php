49dc8e7c3f6b04162ffa396fe0bbefb987c9b316
Alexandr Melnik <wmsystem2008@yandex.ru>
2015-10-26 21:58:59 +0200

    Update OrderAbstract.php

--- src/OrderAbstract.php
+++ src/OrderAbstract.php
@@ -4,6 +4,14 @@ namespace src;
 
 abstract class OrderAbstract
 {
+    /**
+     * @return number
+     */
+    abstract protected function getPrice();
+    
+    /**
+     * @return number
+     */
     protected function calculateDeliveryPrice()
     {
         return $this->getPrice() / 20;
